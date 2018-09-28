5b08dd5d06f59512c94356aa70bc0b5bd30bb50c
Chirilovici Eugeniu <eugen.chirilovici@endava.com>
2018-09-28 12:03:33 +0300

    first commit on yFeature

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,12 @@
 package com.company;
 
-public class Main {
+class ConflictClass{
+    void message(){
+        System.out.println("My conflict, master");
+    }
+}
 
+public class Main {
     public static void main(String[] args) {
         System.out.println("Ce zi minunata :)");
     }
