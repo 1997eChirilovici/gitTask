52dfadf140ce4b38596aa4ac98c236410b5fb6dc
Chirilovici Eugeniu <eugen.chirilovici@endava.com>
2018-09-28 11:50:33 +0300

    first commit on yFeature

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Fish in the sea, you know how I feel");
+        System.out.println("River running free, you know how I feel");
     }
 }
