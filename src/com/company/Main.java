b0df0d966fea32c1652c13775e2e78a45a42fb2e
Chirilovici Eugeniu <eugen.chirilovici@endava.com>
2018-09-28 11:46:51 +0300

    first commit on yFeature

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Ce zi minunata :)");
+        System.out.println("Cu adevart e frumos afara");
     }
 }
