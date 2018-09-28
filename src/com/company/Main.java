43884ca144fa6cdd7dd9d36d97184bbb865cdf43
Chirilovici Eugeniu <eugen.chirilovici@endava.com>
2018-09-28 11:42:07 +0300

    first commit on master

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Ce zi minunata :)");
+        System.out.println("It's a new dawn");
     }
 }
